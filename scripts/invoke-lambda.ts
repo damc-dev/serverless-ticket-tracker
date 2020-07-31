84506a2b3772e858f949fe3c57267f0fe7a49ddd
David McElligott <damc.dev@gmail.com>
2020-07-30 23:41:30 -0500

    Updated invoke-lambda script

--- scripts/invoke-lambda.ts
+++ scripts/invoke-lambda.ts
@@ -1,6 +1,8 @@
 import { spawn } from "child_process";
 import * as path from 'path';
 import * as yargs from 'yargs';
+import * as yaml from 'js-yaml';
+import * as fs from 'fs';
 
 yargs.usage("Runs the sam local invoke on a lambda")
 .options({
@@ -49,11 +51,49 @@ class InvokeLambda {
 async function main(argv: yargs.Arguments): Promise<any> {
     const lambdaDir: string = argv.lambdaDir as string;
     const template: string = argv.template as string;
-    const lambdaLogicalId = "TicketsGet3A5E5442";
-    const envVarsFile = path.resolve(path.join(lambdaDir, "environment.json"))
-    console.log(envVarsFile);
-    const invokeLambda = new InvokeLambda(lambdaLogicalId, template, envVarsFile)
+
+    const lambdaLogicalId = await getLogicalId(template, path.resolve(lambdaDir));
+    if (!lambdaLogicalId) {
+        console.error("Could not find logical id for lambdaDir specified");
+        return;
+    }
+    const envVarsFile = await updateLogicalId(lambdaDir, lambdaLogicalId);
+    const invokeLambda = new InvokeLambda(lambdaLogicalId, template, envVarsFile);
+
     await invokeLambda.invoke();
+    fs.unlinkSync(envVarsFile)
+
+}
+
+async function getLogicalId(template: string, lambdaPath: string): Promise<string> {
+    let logicalId: string = "";
+    try {
+        let fileContents = fs.readFileSync('./template.yaml', 'utf8');
+        let data = yaml.safeLoad(fileContents);
+        let resources = data.Resources;
+        for (const [key, value] of Object.entries(resources)) {
+            let resource: any = value;
+            if (resource.Type === "AWS::Lambda::Function" && resource?.Metadata['aws:asset:path'] === lambdaPath) {
+                logicalId = key;
+            }
+        }
+    } catch (e) {
+        console.log(e);
+    }
+    return logicalId;
+}
+
+async function updateLogicalId(lambdaDir: string, logicalId: string) {
+    let rawdata = fs.readFileSync(path.resolve(path.join(lambdaDir, "environment.json")), {
+        encoding: "UTF-8"
+    });
+    let env = JSON.parse(rawdata);
+    let environment = {
+        [logicalId]: env
+    }
+    let envFile = path.resolve(path.join(lambdaDir, "env.json"))
+    fs.writeFileSync(envFile,  JSON.stringify(environment));
+    return envFile
 }
 
 main(yargs.argv).catch((err) => {
